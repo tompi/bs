3a275ec0f724ded6b6fd96c951e266f1711d33ae
Thomas Haukland <thomas.haukland@gmail.com>
2014-09-08 21:51:11 +0200

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
 var gulp = require('gulp');
+var less = require('gulp-less');
+var path = require('path');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var del = require('del');
 
 // Compile Bootstrap
-gulp.task('build-less', function(){
-  return gulp.src('client/css/styles.less')
-    .pipe(less())
-    .pipe(gulp.dest('client/css'));
+gulp.task('less', function () {
+  gulp.src('./client/**/*.less')
+  .pipe(less({
+    paths: [ path.join(__dirname, 'less', 'includes') ]
+  }))
+  .pipe(gulp.dest('./client/'));
 });
 
-gulp.task('default', ['build-less']);
+gulp.task('default', ['less']);
